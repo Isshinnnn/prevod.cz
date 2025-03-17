65e1281c3f4938379d1d4171d037a5c88453bfad
Isshinnnn <144329318+Isshinnnn@users.noreply.github.com>
2025-03-17 20:25:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,49 +4,42 @@ function convertMetersToYards() {
         document.getElementById("yards").value = (meters * 1.09361).toFixed(2);
     }
 }
-
 function convertYardsToMeters() {
     let yards = parseFloat(document.getElementById("yards").value);
     if (!isNaN(yards)) {
         document.getElementById("meters").value = (yards / 1.09361).toFixed(2);
     }
 }
-
 function convertMetersToFeet() {
     let meters = parseFloat(document.getElementById("meters2").value);
     if (!isNaN(meters)) {
         document.getElementById("feet").value = (meters * 3.28084).toFixed(2);
     }
 }
-
 function convertFeetToMeters() {
     let feet = parseFloat(document.getElementById("feet").value);
     if (!isNaN(feet)) {
         document.getElementById("meters2").value = (feet / 3.28084).toFixed(2);
     }
 }
-
 function convertCentimetersToInches() {
     let centimeters = parseFloat(document.getElementById("centimeters").value);
     if (!isNaN(centimeters)) {
         document.getElementById("inches").value = (centimeters * 0.393701).toFixed(2);
     }
 }
-
 function convertInchesToCentimeters() {
     let inches = parseFloat(document.getElementById("inches").value);
     if (!isNaN(inches)) {
         document.getElementById("centimeters").value = (inches / 0.393701).toFixed(2);
     }
 }
-
 function convertKilometersToMiles() {
     let kilometers = parseFloat(document.getElementById("kilometers").value);
     if (!isNaN(kilometers)) {
         document.getElementById("miles").value = (kilometers * 0.621371).toFixed(2);
     }
 }
-
 function convertMilesToKilometers() {
     let miles = parseFloat(document.getElementById("miles").value);
     if (!isNaN(miles)) {
